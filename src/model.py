b9ab28246caaf94713acd97be19958394f7574c3
tseesurenb <133945855+tseesurenb@users.noreply.github.com>
2024-11-29 17:47:08 +1100

    hi

--- src/model.py
+++ src/model.py
@@ -132,7 +132,7 @@ class hyperGCN(MessagePassing):
           
 # HyperGCN Convolutional Layer
 class hyperGAT(MessagePassing):
-    def __init__(self, edge_attr_mode = 'exp', attr_drop = 0.2, self_loop = False, device = 'cpu', **kwargs):  
+    def __init__(self, edge_attr_mode = 'exp', attr_drop = 0.0, self_loop = False, device = 'cpu', **kwargs):  
         super().__init__(aggr='add')
         
         self.edge_attr_mode = edge_attr_mode
@@ -156,34 +156,20 @@ class hyperGAT(MessagePassing):
           attr_heads.append(F.softmax(linear(edge_attrs), dim=0))  # Apply softmax per head
       return torch.stack(attr_heads, dim=1)  # Shape: [num_edges, num_heads, head_dim]
 
-        
     def forward(self, x, edge_index, edge_attrs, scale):
-        
         
         if self.graph_norms is None:
           # Compute normalization  
           self.edge_index_norm = gcn_norm(edge_index=edge_index, add_self_loops=self.add_self_loops)
           self.graph_norms = self.edge_index_norm[1]
-          
-          print('\n before leaky edge_attrs:', edge_attrs)
-          
-          # lets print a sum of edge_attrs values for a sample source node in edge_index
-          #print(f'\n edge_attrs values of edge_index[0] with number of edges: {edge_attrs[edge_index[0] == 0].sum()}, {edge_attrs[edge_index[0] == 0].shape[0]}')
-          #print(f'\n edge_attrs values of edge_index[0] with number of edges: {edge_attrs[edge_index[0] == 10].sum()}, {edge_attrs[edge_index[0] == 10].shape[0]}')
-          
+
           #self.edge_attrs = F.leaky_relu(torch.exp(scale * edge_attrs), negative_slope=0.2)
           self.edge_attrs = softmax(scale * edge_attrs, edge_index[0])
-          
-          #print(f'\n edge_attrs values of edge_index[0] with number of edges: {self.edge_attrs[edge_index[0] == 0].sum()}, {self.edge_attrs[edge_index[0] == 0].shape[0]}')
-          #print(f'\n edge_attrs values of edge_index[0] with number of edges: {self.edge_attrs[edge_index[0] == 10].sum()}, {self.edge_attrs[edge_index[0] == 10].shape[0]}')
-          
-          print('\n after leaky edge_attrs:', self.edge_attrs)
-          
         else:
           self.edge_attrs = None
         
-                      
-        #self.edge_attrs = edge_attr_drop(edge_index, self.edge_attrs, self.attr_drop)
+        if self.attr_drop > 0.0:
+          edge_attrs = edge_attr_drop(edge_index, edge_attrs, self.attr_drop)
         
         # Start propagating messages (no update after aggregation)
         return self.propagate(edge_index, x=x, norm=self.graph_norms, attr = self.edge_attrs)
@@ -359,7 +345,7 @@ class RecSysGNN(nn.Module):
       n_items,
       model, # 'NGCF' or 'LightGCN' or 'hyperGCN'
       dropout=0.1, # Only used in NGCF
-      attr_drop = 0.2, # Only used in hyperGCN
+      attr_drop = 0.0, # Only used in hyperGCN
       edge_attr_mode = None,
       scale = 1.0,
       device = 'cpu',
